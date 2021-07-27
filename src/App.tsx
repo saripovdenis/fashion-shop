eac1041acdb2422002ce21f0c8edab73b80fc03e
Denis Saripov <bblankbboi@gmail.com>
2021-07-27 14:21:33 +0300

    Added react-routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Route } from 'react-router';
 
 import { Cart } from './components';
 import { Shipping, Billing, Payment } from './pages';
@@ -6,7 +7,9 @@ import { Shipping, Billing, Payment } from './pages';
 const App: React.FC = () => {
   return (
     <div className="app">
-      <Shipping />
+      <Route path="/shipping" component={Shipping} exact />
+      <Route path="/billing" component={Billing} exact />
+      <Route path="/payment" component={Payment} exact />
       <Cart />
     </div>
   );
--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
 
+import { BrowserRouter as Router } from 'react-router-dom';
+
 import './scss/app.scss';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root'),
 );
