557bbdd16b0554850610dfc03e5c43d2a192c89e
Denis Saripov <bblankbboi@gmail.com>
2021-07-30 01:49:54 +0300

    Added a error messages in layout

--- src/pages/Billing.tsx
+++ src/pages/Billing.tsx
@@ -16,32 +16,68 @@ function Billing() {
           <div className="form__block">
             <h2>Billing Contact</h2>
             <div className="form__block__input">
-              <input placeholder="Full Name" type="text" />
+              <div className="form__block__input--full">
+                <div className="form__block__input__error">
+                  <div className="form__block__input__error--message">
+                    Please enter recipient full name
+                  </div>
+                </div>
+                <input placeholder="Full Name" type="text" />
+              </div>
               <div className="form__block__input--medium">
+                <div className="form__block__input__error">
+                  <div className="form__block__input__error--message">
+                    Please enter valid number
+                  </div>
+                </div>
                 <input placeholder="Daytime Phone" type="text" />
+                <label>For delivery questions only</label>
               </div>
             </div>
           </div>
           <div className="form__block">
             <h2>Billing Address</h2>
             <div className="form__block__input">
-              <input placeholder="Street Address" type="text" />
-              <br />
-              <input placeholder="Apt, Suite, Bldg, Gate Code. (optional)" type="text" />
+              <div className="form__block__input--full">
+                <div className="form__block__input__error">
+                  <div className="form__block__input__error--message">
+                    Please enter valid address
+                  </div>
+                </div>
+                <input placeholder="Street Address" type="text" />
+              </div>
+              <div className="form__block__input--full">
+                <input placeholder="Apt, Suite, Bldg, Gate Code. (optional)" type="text" />
+              </div>
               <div className="out">
-                <input placeholder="City" type="text" />
+                <div className="form__block__input--full">
+                  <div className="form__block__input__error">
+                    <div className="form__block__input__error--message">
+                      Please enter valid city
+                    </div>
+                  </div>
+                  <input placeholder="City" type="text" />
+                </div>
               </div>
               <div className="out">
                 <div className="form__block__input--medium">
+                  <div className="form__block__input__error">
+                    <div className="form__block__input__error--message">
+                      Please enter valid country
+                    </div>
+                  </div>
                   <input placeholder="Country" type="text" />
                 </div>
                 <div className="form__block__input--short">
+                  <div className="form__block__input__error">
+                    <div className="form__block__input__error--message">Please enter valid ZIP</div>
+                  </div>
                   <input placeholder="ZIP" type="text" />
                 </div>
               </div>
             </div>
-            <button>Continue</button>
           </div>
+          <button>Continue</button>
         </form>
       </div>
     </div>
--- src/pages/Payment.tsx
+++ src/pages/Payment.tsx
@@ -16,13 +16,25 @@ function Payment() {
           <div className="form__block">
             <h2>Cardholder Name</h2>
             <div className="form__block__input">
-              <input placeholder="Name as it appears on your card" type="text" name="" id="" />
+              <div className="form__block__input--full">
+                <div className="form__block__input__error">
+                  <div className="form__block__input__error--message">Please enter valid name</div>
+                </div>
+                <input placeholder="Name as it appears on your card" type="text" name="" id="" />
+              </div>
             </div>
           </div>
           <div className="form__block">
             <h2>Card Number</h2>
             <div className="form__block__input">
-              <input placeholder="XXXX XXXX XXXX XXXX XXXX" type="text" name="" id="" />
+              <div className="form__block__input--full">
+                <div className="form__block__input__error">
+                  <div className="form__block__input__error--message">
+                    Please enter valid number
+                  </div>
+                </div>
+                <input placeholder="XXXX XXXX XXXX XXXX XXXX" type="text" name="" id="" />
+              </div>
             </div>
           </div>
           <div className="form__block">
@@ -31,6 +43,11 @@ function Payment() {
                 <h2>Expire Date</h2>
                 <div className="form__block__input">
                   <div className="form__block__input--short">
+                    <div className="form__block__input__error">
+                      <div className="form__block__input__error--message">
+                        Please enter valid date
+                      </div>
+                    </div>
                     <input placeholder="MM/YY" type="text" />
                   </div>
                 </div>
@@ -39,6 +56,11 @@ function Payment() {
                 <h2>Security Code</h2>
                 <div className="form__block__input">
                   <div className="form__block__input--short">
+                    <div className="form__block__input__error">
+                      <div className="form__block__input__error--message">
+                        Please enter valid code
+                      </div>
+                    </div>
                     <input placeholder="XXX" type="text" />
                   </div>
                 </div>
